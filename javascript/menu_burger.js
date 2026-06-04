38809b84591cc7bd585a24d4ee0ee99cfcd719ea
Morgan-Saturn <morgan.tunteva@proton.me>
2026-06-04 10:41:31 +0200

    made the burger navbar take more space when active for easier use

--- javascript/menu_burger.js
+++ javascript/menu_burger.js
@@ -2,14 +2,17 @@ let icons = document.getElementById("icons");
 console.log(icons);
 const nav = document.getElementById("nav");
 const liens = document.querySelectorAll("nav li");
+let iframe = document.querySelector("iframe");
 
 icons.addEventListener("click", () => {
-    nav.classList.toggle("active")
+    nav.classList.toggle("active");
+    iframe.classList.toggle("menu_actif")
 });
 
 liens.forEach((lien) => {
     lien.addEventListener("click", () => {
         nav.classList.remove("active");
+        iframe.classList.remove("menu_actif");
     })
 })
 //forEach pour ne pas faire d'eventlistener pour chaque lien//
