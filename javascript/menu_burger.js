c1403db39c1faff753624ecd9bdba22949ae5bdf
Morgan-Saturn <morgan.tunteva@proton.me>
2026-06-04 11:44:17 +0200

    fixed the burger icons that didn't work anymore

--- javascript/menu_burger.js
+++ javascript/menu_burger.js
@@ -1,18 +1,19 @@
-let icons = document.getElementById("icons");
-console.log(icons);
+let menu_button = document.getElementById("menu_button");
 const nav = document.getElementById("nav");
 const liens = document.querySelectorAll("nav li");
 let iframe = document.querySelector("iframe");
 
-icons.addEventListener("click", () => {
+menu_button.addEventListener("click", () => {
     nav.classList.toggle("active");
     iframe.classList.toggle("menu_actif")
+    menu_button.classList.toggle("bouton_actif")
 });
 
 liens.forEach((lien) => {
     lien.addEventListener("click", () => {
         nav.classList.remove("active");
         iframe.classList.remove("menu_actif");
+        menu_button.classList.remove("bouton_actif")
     })
 })
 //forEach pour ne pas faire d'eventlistener pour chaque lien//
